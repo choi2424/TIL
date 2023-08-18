7a986f82e5b377d7234c79f97c2398db9f557dea
choi2424 <134917568+choi2424@users.noreply.github.com>
2023-08-18 15:47:48 +0900

    Add files via upload

---
+++ react/nextapp/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+// return 값이 layout.js의 children 변수에 삽입된다.
+export default function Home() {
+  return (
+    <>
+      <h2>Welcom</h2>
+      Hello,WEB
+    </> 
+  )
+}
+  
