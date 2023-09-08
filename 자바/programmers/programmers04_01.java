cf69a5096d75116ddc8b2b5ed106eac5dd74016c
choi2424 <134917568+choi2424@users.noreply.github.com>
2023-09-08 15:47:51 +0900

    Add files via upload

---
+++ 자바/programmers/programmers04_01.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class programmers04_01 {
+
+	public static void main(String[] args) {
+//		정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return 
+//		n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+		
+		int num = 98;
+		int n = 2;
+		int answer = 0;
+		
+		answer = (num % n == 0) ? 1 : 0;
+		System.out.println(answer);
+			
+	}
+
+}
